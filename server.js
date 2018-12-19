9fd2b1f5bb9304f61d7083c78cdb6039efbd2648
RookieYang <yy335281965@gmail.com>
2018-12-19 16:58:25 +0800

    添加post请求

--- server.js
+++ server.js
@@ -14,27 +14,38 @@ var http = require('http')
  
   //从这里开始看，上面不要看
    if(path === '/'){  // 如果用户请求的是 / 路径
-    var string = fs.readFileSync('./index.html')  
+
     var string = fs.readFileSync('./index.html', 'utf8')  
+    var amount = fs.readFileSync('./db','utf8')
+    string = string.replace('&&&amount&&&',amount)
     response.setHeader('Content-Type', 'text/html;charset=utf-8')  
     response.write(string)   
     response.end()
   }else if(path === '/style.css'){   
-    var string = fs.readFileSync('./style.css')
     var string = fs.readFileSync('./style.css', 'utf8')
     response.setHeader('Content-Type', 'text/css')
     response.write(string)
     response.end()
   }else if(path === '/main.js'){  
-    var string = fs.readFileSync('./main.js')
     var string = fs.readFileSync('./main.js', 'utf8')
     response.setHeader('Content-Type', 'application/javascript')
     response.write(string)
     response.end()
+  }else if(path === '/pay' && method.toUpperCase() ==='POST'){
+
+    var amount = fs.readFileSync('./db','utf8')
+    var newAmount = amount - 1
+    if(Math.random()>0.5){
+      fs.writeFileSync('./db',newAmount)
+      response.write('success')
+    }else{
+      response.write('fail')
+    }
+    response.end()
   }else{   
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8') 
-    response.write('找不到对应的路径，你需要自行修改 index.js')
+    response.write('找不到对应的路径，你需要自行修改 server.js')
     response.end()
   }
 
